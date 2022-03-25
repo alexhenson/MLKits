1d5e68dd1cc4cd963fece357ab6d30e62a52887a
henson-alex <henson.alex@ibm.com>
2022-03-25 16:42:51 -0500

    init setup of index.js

--- plinko/score.js
+++ plinko/score.js
@@ -8,16 +8,21 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 
 function runAnalysis() {
   const testSetSize = 100;
-  const [testSet, trainingSet] = splitDataset(minMax(outputs, 3), testSetSize);
+  const k = 10;
 
-  _.range(1, 20).forEach((k) => {
+  _.range(0, 3).forEach((feature) => {
+    const data = _.map(outputs, (row) => [row[feature], _.last(row)]);
+    const [testSet, trainingSet] = splitDataset(minMax(data, 1), testSetSize);
     const accuracy = _.chain(testSet)
-      .filter((testPoint) => knn(trainingSet, _.initial(testPoint), k) === testPoint[3])
+      .filter(
+        (testPoint) =>
+          knn(trainingSet, _.initial(testPoint), k) === _.last(testPoint)
+      )
       .size()
       .divide(testSetSize)
       .value();
 
-    console.log('For k of', k, 'Accuracy is', accuracy);
+    console.log('For feature of', feature, 'Accuracy is', accuracy);
   });
 }
 
@@ -61,7 +66,7 @@ function minMax(data, featureCount) {
   const clonedData = _.cloneDeep(data);
 
   for (let i = 0; i < featureCount; i++) {
-    const column = clonedData.map(row => row[i]);
+    const column = clonedData.map((row) => row[i]);
 
     const min = _.min(column);
     const max = _.max(column);
