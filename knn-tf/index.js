f60df70518c7143e895b10b1a3d96fde9d542543
Alex Henson <40705295+alexhenson@users.noreply.github.com>
2022-03-26 16:01:45 -0500

    installing dependencies

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -1,11 +1,17 @@
-require('@tensorflow/tfjs-node');
-const tf = require('@tensorflow/tfjs');
-const loadCSV = require('./load-csv');
+require("@tensorflow/tfjs-node");
+const tf = require("@tensorflow/tfjs");
+const loadCSV = require("./load-csv");
 
 function knn(features, labels, predictionPoint, k) {
+  const { mean, variance } = tf.moments(features, 0);
+
+  const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(0.5));
+
   return (
     features
-      .sub(predictionPoint)
+      .sub(mean)
+      .div(variance.pow(0.5))
+      .sub(scaledPrediction)
       .pow(2)
       .sum(1)
       .pow(0.5)
@@ -19,27 +25,24 @@ function knn(features, labels, predictionPoint, k) {
 }
 
 let { features, labels, testFeatures, testLabels } = loadCSV(
-  'kc_house_data.csv',
+  "kc_house_data.csv",
   {
     shuffle: true,
     splitTest: 10,
-    dataColumns: ['lat', 'long', 'sqft_lot'],
-    labelColumns: ['price'],
+    dataColumns: ["lat", "long", "sqft_lot"],
+    labelColumns: ["price"],
   }
 );
 
-console.log('Test features', testFeatures);
-console.log('Test labels', testLabels);
+// console.log("Test features", testFeatures);
+// console.log("Test labels", testLabels);
 
 features = tf.tensor(features);
 labels = tf.tensor(labels);
 
 testFeatures.forEach((testPoint, i) => {
   const result = knn(features, labels, tf.tensor(testPoint), 10);
-  const err = (testLabels[i][0]- result) / testLabels[i][0];
-  console.log('Guess', result, testLabels[i][0])
-  console.log('Error', err * 100)
-})
-
-
-
+  const err = (testLabels[i][0] - result) / testLabels[i][0];
+  // console.log("Guess", result, testLabels[i][0]);
+  console.log("Error", err * 100);
+});
