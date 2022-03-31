f29485bcf10ac0aae63cf7cdbb6fd47f1c84d614
henson-alex <henson.alex@ibm.com>
2022-03-31 10:01:53 -0500

    finished inital linear regression model class

--- regressions/index.js
+++ regressions/index.js
@@ -1,6 +1,7 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
+const LinearRegression = require('./linear-regression');
 
 let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
   shuffle: true,
@@ -9,4 +10,10 @@ let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
   labelColumns: ['mpg'],
 });
 
+const regression = new LinearRegression(features, labels, {
+  learningRate: 0.001,
+  iterations: 1,
+});
+
+regression.train();
 // console.log(features, labels);
