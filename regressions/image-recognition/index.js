19f4777920f6631fecbde24784ff0b5f2589cc9c
henson-alex <henson.alex@ibm.com>
2022-04-07 18:43:45 -0500

    completed optimizatioin for logistic regression

--- regressions/image-recognition/index.js
+++ regressions/image-recognition/index.js
@@ -6,10 +6,10 @@ const _ = require('lodash');
 const mnist = require('mnist-data');
 
 function loadData() {
-  const mnistData = mnist.training(0, 20000);
+  const mnistData = mnist.training(0, 60000);
 
-  const features = mnistData.images.values.map(image => _.flatMap(image));
-  const encodedLabels = mnistData.labels.values.map(label => {
+  const features = mnistData.images.values.map((image) => _.flatMap(image));
+  const encodedLabels = mnistData.labels.values.map((label) => {
     const row = new Array(10).fill(0);
     row[label] = 1;
     return row;
@@ -21,16 +21,18 @@ function loadData() {
 const { features, labels } = loadData();
 
 const regression = new LogisticRegression(features, labels, {
-  learningRate: 1, 
-  iterations: 20, 
-  batchSize: 100
+  learningRate: 1,
+  iterations: 40,
+  batchSize: 500,
 });
 
 regression.train();
-debugger;
-const testMnistData = mnist.testing(0, 1000);
-const testFeatures = testMnistData.images.values.map(image => _.flatMap(image));
-const testEncodedLabels = testMnistData.labels.values.map(label => {
+
+const testMnistData = mnist.testing(0, 10000);
+const testFeatures = testMnistData.images.values.map((image) =>
+  _.flatMap(image)
+);
+const testEncodedLabels = testMnistData.labels.values.map((label) => {
   const row = new Array(10).fill(0);
   row[label] = 1;
   return row;
@@ -38,3 +40,7 @@ const testEncodedLabels = testMnistData.labels.values.map(label => {
 
 const accuracy = regression.test(testFeatures, testEncodedLabels);
 console.log('Accuracy is', accuracy);
+
+plot({
+  x: regression.costHistory.reverse(),
+});
